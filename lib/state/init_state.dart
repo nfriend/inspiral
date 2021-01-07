0178eb3cac4b2949e266f711400afc3f031a0254
Nathan Friend <nathan@gitlab.com>
2021-01-07 09:11:15 -0500

    fix: fix initial zoom level

--- lib/state/init_state.dart
+++ lib/state/init_state.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:inspiral/models/models.dart';
 import 'package:vector_math/vector_math_64.dart';
 import 'package:flutter/material.dart';
@@ -14,23 +15,29 @@ void initState(BuildContext context) {
   // the screen's top-left corner
   final Matrix4 initialCanvasTransform = Matrix4.identity();
 
-  // First, move the center of the canvas to the
+  // Scale the canvas to the correct zoom level
+  final initialZoom = 0.5;
+  initialCanvasTransform.scale(initialZoom, initialZoom, 0);
+
+  // Move the center of the canvas to the
   // top-left of the screen
-  Vector3 translation = -(canvasCenter.toVector3());
+  Vector3 originTranslation = -(canvasCenter.toVector3());
+  initialCanvasTransform.translate(originTranslation);
 
   // Then, move the canvas back by half the screen dimensions
   // so that the centor of the canvas is located
-  // in the centr of the screen
-  translation += (MediaQuery.of(context).size / 2).toVector3();
-
-  initialCanvasTransform.translate(translation);
+  // in the center of the screen
+  Vector3 centerTranslation =
+      (MediaQuery.of(context).size / 2).toVector3() * (1 / initialZoom);
+  initialCanvasTransform.translate(centerTranslation);
 
   // Initialize all the state singletons
   SettingsState.init();
   final pointers = PointersState.init();
   final canvas = CanvasState.init(initialTransform: initialCanvasTransform);
   final rotatingGear = RotatingGearState.init(
-      initialAngle: 0, initialDefinition: GearDefinitions.defaultRotatingGear);
+      initialAngle: pi / 2,
+      initialDefinition: GearDefinitions.defaultRotatingGear);
   final dragLine = DragLineState.init(initialPosition: canvasCenter);
   final fixedGear = FixedGearState.init(
       initialPosition: canvasCenter,
