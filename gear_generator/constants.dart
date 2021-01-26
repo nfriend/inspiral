161b15e4bc408ee009bd1afe66085b2f8a1ea811
Nathan Friend <nathan@gitlab.com>
2021-01-25 20:03:24 -0500

    refactor: remove gear_generator directory

--- gear_generator/constants.dart
+++
@@ -1,5 +0,0 @@
-/// The height of each tooth
-const int toothHeight = 5;
-
-/// How many pixels should be rendered for each SVG unit
-const double scale = 8;
--- gear_generator/extensions/extensions.dart
+++
@@ -1,1 +0,0 @@
-export './point_extensions.dart';
