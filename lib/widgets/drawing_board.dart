11b52f88b517c4c0bfde688ece8159dca2e44ace
Nathan Friend <nathan@gitlab.com>
2021-03-21 18:40:42 -0400

    fix: iOS fixes - safe area and photo gallery

--- lib/widgets/drawing_board.dart
+++ lib/widgets/drawing_board.dart
@@ -17,7 +17,9 @@ import 'package:tinycolor/tinycolor.dart';
 class DrawingBoard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIOverlays([]);
+    SystemChrome.setEnabledSystemUIOverlays([
+      SystemUiOverlay.top,
+    ]);
 
     final fixedGear = Provider.of<FixedGearState>(context, listen: false);
     final rotatingGear = Provider.of<RotatingGearState>(context, listen: false);
@@ -28,42 +30,45 @@ class DrawingBoard extends StatelessWidget {
     final bool debug =
         context.select<SettingsState, bool>((settings) => settings.debug);
 
-    final Widget scaffoldBody = Listener(
-        behavior: HitTestBehavior.translucent,
-        onPointerDown: (event) {
-          pointers.pointerDown(event);
-          fixedGear.globalPointerDown(event);
-          rotatingGear.globalPointerDown(event);
-        },
-        onPointerMove: (event) {
-          pointers.pointerMove(event);
-        },
-        onPointerUp: (event) {
-          pointers.pointerUp(event);
-          canvas.globalPointerUp(event);
-        },
-        child: Stack(children: [
-          // This `OverflowBox` should be twice the size of the canvas.
-          // The canvas will be centered in `OverflowBox`, leaving a large
-          // amount of padding on every side. This padding allows the gears
-          // to respond to pointer events even when outside the canvas.
-          OverflowBox(
-            maxHeight: canvasSize.height * 2,
-            minHeight: canvasSize.height * 2,
-            maxWidth: canvasSize.width * 2,
-            minWidth: canvasSize.width * 2,
-            alignment: Alignment.topLeft,
-            child: Container(
-                color: appBackgroundColor.color, child: CanvasContainer()),
-          ),
-          Positioned(
-              left: 0,
-              right: 0,
-              top: 0,
-              child: AnimatedToolbarContainer(
-                  translateY: -42.0, child: MenuBar())),
-          Positioned(left: 0, right: 0, bottom: 0, child: SelectorDrawer())
-        ]));
+    final double safePaddingTop = MediaQuery.of(context).padding.top;
+
+    final Widget scaffoldBody = SafeArea(
+        child: Listener(
+            behavior: HitTestBehavior.translucent,
+            onPointerDown: (event) {
+              pointers.pointerDown(event);
+              fixedGear.globalPointerDown(event);
+              rotatingGear.globalPointerDown(event);
+            },
+            onPointerMove: (event) {
+              pointers.pointerMove(event);
+            },
+            onPointerUp: (event) {
+              pointers.pointerUp(event);
+              canvas.globalPointerUp(event);
+            },
+            child: Stack(children: [
+              // This `OverflowBox` should be twice the size of the canvas.
+              // The canvas will be centered in `OverflowBox`, leaving a large
+              // amount of padding on every side. This padding allows the gears
+              // to respond to pointer events even when outside the canvas.
+              OverflowBox(
+                maxHeight: canvasSize.height * 2,
+                minHeight: canvasSize.height * 2,
+                maxWidth: canvasSize.width * 2,
+                minWidth: canvasSize.width * 2,
+                alignment: Alignment.topLeft,
+                child: Container(
+                    color: appBackgroundColor.color, child: CanvasContainer()),
+              ),
+              Positioned(
+                  left: 0,
+                  right: 0,
+                  top: 0,
+                  child: AnimatedToolbarContainer(
+                      translateY: -42.0 - safePaddingTop, child: MenuBar())),
+              Positioned(left: 0, right: 0, bottom: 0, child: SelectorDrawer())
+            ])));
 
     return DynamicTheme(
         child: ModalProgress(
