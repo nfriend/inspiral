480aa0b3dcf98df69231b6f281d95f9e7fa6ef67
Nathan Friend <nathan@gitlab.com>
2021-03-23 23:57:12 -0400

    fix: fullscreen fixes for iOS and Android

--- lib/inspiral_app.dart
+++ lib/inspiral_app.dart
@@ -1,4 +1,7 @@
+import 'dart:io' as io;
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:inspiral/constants.dart';
 import 'package:inspiral/widgets/inspiral_providers.dart';
 import 'package:inspiral/routes.dart';
@@ -7,6 +10,28 @@ import 'package:inspiral/widgets/drawing_board.dart';
 class InspiralApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Test whether or not the phone has a notch
+    bool hasNotch;
+    if (io.Platform.isIOS) {
+      // Not exactly sure why/how this works, but it does :shrug:
+      // See https://stackoverflow.com/a/58849024/1063392
+      hasNotch =
+          ui.window.viewPadding.top > 0 && ui.window.viewPadding.bottom > 0;
+    } else {
+      // I haven't figured out a reliable way to detect notches in Android yet
+      hasNotch = false;
+    }
+
+    // Hide the top status bar for phones that _don't_ have a notch
+    if (hasNotch) {
+      SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
+    } else {
+      SystemChrome.setEnabledSystemUIOverlays([]);
+    }
+
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
     return MaterialApp(
       scaffoldMessengerKey: scaffoldGlobalKey,
       title: 'Inspiral',
--- lib/widgets/drawing_board.dart
+++ lib/widgets/drawing_board.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter/services.dart';
 import 'package:inspiral/widgets/animated_toolbar_container.dart';
 import 'package:inspiral/widgets/canvas_container.dart';
 import 'package:inspiral/widgets/drawing_tools/drawing_tools.dart';
@@ -17,12 +16,6 @@ import 'package:tinycolor/tinycolor.dart';
 class DrawingBoard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // SystemChrome.setEnabledSystemUIOverlays(
-    //     [SystemUiOverlay.top, SystemUiOverlay.bottom]);
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarColor: Colors.black,
-    ));
-
     final fixedGear = Provider.of<FixedGearState>(context, listen: false);
     final rotatingGear = Provider.of<RotatingGearState>(context, listen: false);
     final pointers = Provider.of<PointersState>(context, listen: false);
@@ -34,43 +27,42 @@ class DrawingBoard extends StatelessWidget {
 
     final double safePaddingTop = MediaQuery.of(context).padding.top;
 
-    final Widget scaffoldBody = SafeArea(
-        child: Listener(
-            behavior: HitTestBehavior.translucent,
-            onPointerDown: (event) {
-              pointers.pointerDown(event);
-              fixedGear.globalPointerDown(event);
-              rotatingGear.globalPointerDown(event);
-            },
-            onPointerMove: (event) {
-              pointers.pointerMove(event);
-            },
-            onPointerUp: (event) {
-              pointers.pointerUp(event);
-              canvas.globalPointerUp(event);
-            },
-            child: Stack(children: [
-              // This `OverflowBox` should be twice the size of the canvas.
-              // The canvas will be centered in `OverflowBox`, leaving a large
-              // amount of padding on every side. This padding allows the gears
-              // to respond to pointer events even when outside the canvas.
-              OverflowBox(
-                maxHeight: canvasSize.height * 2,
-                minHeight: canvasSize.height * 2,
-                maxWidth: canvasSize.width * 2,
-                minWidth: canvasSize.width * 2,
-                alignment: Alignment.topLeft,
-                child: Container(
-                    color: appBackgroundColor.color, child: CanvasContainer()),
-              ),
-              Positioned(
-                  left: 0,
-                  right: 0,
-                  top: 0,
-                  child: AnimatedToolbarContainer(
-                      translateY: -42.0 - safePaddingTop, child: MenuBar())),
-              Positioned(left: 0, right: 0, bottom: 0, child: SelectorDrawer())
-            ])));
+    final Widget scaffoldBody = Listener(
+        behavior: HitTestBehavior.translucent,
+        onPointerDown: (event) {
+          pointers.pointerDown(event);
+          fixedGear.globalPointerDown(event);
+          rotatingGear.globalPointerDown(event);
+        },
+        onPointerMove: (event) {
+          pointers.pointerMove(event);
+        },
+        onPointerUp: (event) {
+          pointers.pointerUp(event);
+          canvas.globalPointerUp(event);
+        },
+        child: Stack(children: [
+          // This `OverflowBox` should be twice the size of the canvas.
+          // The canvas will be centered in `OverflowBox`, leaving a large
+          // amount of padding on every side. This padding allows the gears
+          // to respond to pointer events even when outside the canvas.
+          OverflowBox(
+            maxHeight: canvasSize.height * 2,
+            minHeight: canvasSize.height * 2,
+            maxWidth: canvasSize.width * 2,
+            minWidth: canvasSize.width * 2,
+            alignment: Alignment.topLeft,
+            child: Container(
+                color: appBackgroundColor.color, child: CanvasContainer()),
+          ),
+          Positioned(
+              left: 0,
+              right: 0,
+              top: safePaddingTop,
+              child: AnimatedToolbarContainer(
+                  translateY: -42.0 - safePaddingTop, child: MenuBar())),
+          Positioned(left: 0, right: 0, bottom: 0, child: SelectorDrawer())
+        ]));
 
     return DynamicTheme(
         child: ModalProgress(
