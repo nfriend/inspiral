30338322904f17917bc517f1469715f4a8480a73
Nathan Friend <nathan@gitlab.com>
2021-04-06 10:57:11 -0400

    feat: Sentry integration
    Closes #32

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,26 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:inspiral/inspiral_app.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
-void main() {
-  runApp(InspiralApp());
+Future<void> main() async {
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://4533347562a94db1a09a0308e3bed4f0@o403829.ingest.sentry.io/5707774';
+
+      // See https://stackoverflow.com/a/61725261/1063392 for instructions
+      // on building this app with the `IS_PRODUCTION` var defined.
+      if (String.fromEnvironment('IS_PRODUCTION', defaultValue: '') != '') {
+        options.environment = 'production';
+      } else if (kReleaseMode) {
+        options.environment = 'release';
+      } else if (kProfileMode) {
+        options.environment = 'profile';
+      } else {
+        options.environment = 'debug';
+      }
+    },
+    appRunner: () => runApp(InspiralApp()),
+  );
 }
