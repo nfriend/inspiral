94a2338ec43e62c35a676c97e5963ab50d705937
Nathan Friend <nathan@gitlab.com>
2021-04-22 09:57:36 -0400

    refactor: add "clean" task

---
+++ gear_generator/src/clean.ts
@@ -0,0 +1,47 @@
+import fs from 'fs';
+import util from 'util';
+import chalk from 'chalk';
+import globSync from 'glob';
+
+const glob = util.promisify(globSync);
+const deleteFile = util.promisify(fs.unlink);
+
+(async () => {
+  // A list of all auto-generated files created
+  // by the gear generation process
+  const patternsToDelete = [
+    'src/svg/circle_*.svg',
+    'src/svg/oval_*.svg',
+    'src/svg/triangle_*.svg',
+    'src/svg/square_*.svg',
+    'src/svg/pentagon_*.svg',
+    'tmp/**/!(.gitkeep)',
+    '../lib/models/gears/!(README.md)',
+  ];
+
+  let deleteCount = 0;
+  for (const pattern of patternsToDelete) {
+    console.info(chalk.blueBright(`Deleting files in "${pattern}"`));
+
+    const files = await glob(pattern);
+
+    if (files.length === 0) {
+      console.warn(
+        chalk.yellow(`⚠️  Warning: pattern "${pattern}" matched 0 files`),
+      );
+    }
+
+    for (const file of files) {
+      await deleteFile(file);
+      deleteCount++;
+    }
+  }
+
+  console.info(
+    chalk.greenBright(`Successfully deleted ${deleteCount} files 👍`),
+  );
+})().catch((e) => {
+  console.error(chalk.redBright('⚠️  Something went wrong while cleaning!'));
+  console.error(e);
+  process.exit(1);
+});
---
+++ lib/models/gears/README.md
@@ -0,0 +1,5 @@
+# Gears
+
+All files in this directory are auto-generated. Please do not edit manually.
+Everything in this directory (except for this file) is deleted and re-created
+when the gears are regenerated.
