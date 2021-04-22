10d393edd85cc6cd7bd3b29330a7c5d9222cb46b
Nathan Friend <nathan@gitlab.com>
2021-04-22 11:02:19 -0400

    fix: update default selected gear

--- lib/state/persistors/fixed_gear_state_persistor.dart
+++ lib/state/persistors/fixed_gear_state_persistor.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:inspiral/database/schema.dart';
 import 'package:inspiral/models/gear_definition.dart';
 import 'package:inspiral/models/gears/gears.dart';
-import 'package:inspiral/models/gears/oval_30.dart';
 import 'package:inspiral/state/persistors/base_gear_state_rehydration_result.dart';
 import 'package:inspiral/state/state.dart';
 import 'package:sqflite/sqlite_api.dart';
@@ -36,10 +35,10 @@ class FixedGearStatePersistor {
     Map<String, dynamic> state =
         (await db.query(Schema.state.toString())).first;
 
-    // Defaulting to oval30 just in case we didn't find a match in `allGears`.
+    // Defaulting to oval36 just in case we didn't find a match in `allGears`.
     // This _should_ never happen.
     var definition =
-        allGears[state[Schema.state.fixedGearDefinitionId]] ?? oval30;
+        allGears[state[Schema.state.fixedGearDefinitionId]] ?? oval36;
 
     // If these are null, it means we don't have a previously-saved gear
     // position. (For example, when opening the app for the very first time.)
