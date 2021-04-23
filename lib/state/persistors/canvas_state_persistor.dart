377c27521a51aa5e9920703ddd1cb5c945201444
Nathan Friend <nathan@gitlab.com>
2021-04-23 10:48:40 -0400

    feat: better initial zoom
    Closes #66

--- lib/state/persistors/canvas_state_persistor.dart
+++ lib/state/persistors/canvas_state_persistor.dart
@@ -92,7 +92,7 @@ class CanvasStatePersistor {
       transform = getCenterTransform(
           canvasSize: canvasSizeAndName.size,
           screenSize: MediaQuery.of(context).size,
-          initialScale: 0.5);
+          initialScale: 0.3);
     } else {
       transform = Matrix4.fromList(elements);
     }
