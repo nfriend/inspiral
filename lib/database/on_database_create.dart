7ee2e9a50542b2e92d085099564b6b2b4058c388
Nathan Friend <nathan@gitlab.com>
2021-04-24 10:17:37 -0400

    feat: add additional colors to base set
    Closes #77

--- lib/database/on_database_create.dart
+++ lib/database/on_database_create.dart
@@ -40,24 +40,48 @@ void _createTableColorsV1(Batch batch) {
       "${Schema.colors.order}"
     )
     VALUES
+      /* Red */
       ('ab91b67b-e1f0-4d45-9d99-d67383e23bea', '66FF0000', '${ColorsTableType.pen}', 1),
+      /* Orange */
       ('45b3ed99-ef1d-465c-a682-4afc5eac5727', 'B3FF9500', '${ColorsTableType.pen}', 2),
+      /* Yellow */
       ('3a98aacf-47b7-4636-8e9e-f4d0a52b6ae9', 'B3FFFF00', '${ColorsTableType.pen}', 3),
+      /* Green */
       ('2f3e09e1-727e-455b-a5cc-8559e9e9f8ea', '80009600', '${ColorsTableType.pen}', 4),
-      ('7513e7ed-7c49-4dc5-9ee7-fac49b8ffb17', '660000FF', '${ColorsTableType.pen}', 5),
-      ('dcc145c5-e904-4456-983c-78ec0326ab20', '80960096', '${ColorsTableType.pen}', 6),
-      ('5555409d-2e9c-4ddb-979d-df4f71196f0d', 'CCFFFFFF', '${ColorsTableType.pen}', 7),
-      ('bf95f1f6-0ae4-49d9-b992-36b343ddd36c', 'CCC8C8C8', '${ColorsTableType.pen}', 8),
-      ('7e69cc83-c916-417c-82fc-5207937ca2cc', 'CC969696', '${ColorsTableType.pen}', 9),
-      ('11073ff1-6a7e-456c-8276-07e3a87dd3b5', 'CC646464', '${ColorsTableType.pen}', 10),
+      /* Light blue */
+      ('d59187e5-44de-43b3-a244-54dc0aa8ca1e', 'B392d4de', '${ColorsTableType.pen}', 5),
+      /* Blue */
+      ('7513e7ed-7c49-4dc5-9ee7-fac49b8ffb17', '660000FF', '${ColorsTableType.pen}', 6),
+      /* Purple */
+      ('dcc145c5-e904-4456-983c-78ec0326ab20', '80960096', '${ColorsTableType.pen}', 7),
+      /* Pink */
+      ('4a8e131d-5ee4-41f5-b302-0ceebdaa4ee3', 'B3F0A3BA', '${ColorsTableType.pen}', 8),
+      /* Brown */
+      ('afe32b1e-59c9-483a-b5d4-f0fff028f17f', '96401b13', '${ColorsTableType.pen}', 9),
+      /* White */
+      ('5555409d-2e9c-4ddb-979d-df4f71196f0d', 'CCFFFFFF', '${ColorsTableType.pen}', 10),
+      /* Light gray */
+      ('bf95f1f6-0ae4-49d9-b992-36b343ddd36c', 'CCC8C8C8', '${ColorsTableType.pen}', 11),
+      /* Medium gray */
+      ('7e69cc83-c916-417c-82fc-5207937ca2cc', 'CC969696', '${ColorsTableType.pen}', 12),
+      /* Dark gray */
+      ('11073ff1-6a7e-456c-8276-07e3a87dd3b5', 'CC646464', '${ColorsTableType.pen}', 13),
 
+      /* White */
       ('e243e24c-17be-40fa-8505-091a0f2e2a03', 'FFFFFFFF', '${ColorsTableType.canvas}', 1),
+      /* Light gray */
       ('dd8fb49b-1b4c-448c-8f1a-54c3ac2018ec', 'FFF0F0F0', '${ColorsTableType.canvas}', 2),
+      /* Medium-light gray */
       ('d156c05c-131d-4fec-8514-f11324ba4f4e', 'FFE3E3E3', '${ColorsTableType.canvas}', 3),
+      /* Beige */
       ('d36b3350-8a2c-4bd9-9860-1f2e756a4f35', 'FFF7EFDA', '${ColorsTableType.canvas}', 4),
+      /* Brown */
       ('0aa3ca97-6ac6-4178-a4ef-3a97afe2d1fc', 'FF3B2507', '${ColorsTableType.canvas}', 5),
+      /* Navy */
       ('d1248032-9064-43fd-8f26-f1960b572fe1', 'FF0E1247', '${ColorsTableType.canvas}', 6),
+      /* Medium gray */
       ('6d06e1ce-4118-4b95-b266-0b125ce6532a', 'FF333333', '${ColorsTableType.canvas}', 7),
+      /* Dark gray */
       ('487682ec-990e-46cd-a969-b8117d818413', 'FF121212', '${ColorsTableType.canvas}', 8),
 
       ('97a24b56-edc6-4613-beeb-f9a139f7f669', 'B348F1F7', '${ColorsTableType.lastSelectedPen}', 1),
