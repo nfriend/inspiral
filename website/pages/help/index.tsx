f9bdf2cfc2bc2c90f00d04d1304729d402161759
Nathan Friend <nathan@gitlab.com>
2021-05-09 07:26:23 -0400

    docs: add additional help content

--- website/pages/help/index.tsx
+++ website/pages/help/index.tsx
@@ -2,17 +2,46 @@ import HelpCustomHead from './help-custom-head';
 
 export default function Home() {
   return (
-    <div className="help w-screen h-screen p-10 flex justify-center">
+    <div className="help w-screen h-screen p-10 flex justify-center font-sans">
       <div className="max-w-xl">
         <HelpCustomHead />
+
+        <div className="alert alert-info">
+          <p>
+            <b>Welcome to the Inspiral app's documentation!</b>
+          </p>
+
+          <p>
+            This content is a work-in-progress and is not complete, but will
+            hopefully answer some of the most common questions.
+          </p>
+        </div>
 
         <h1>Table of contents</h1>
 
         <ul className="list-disc list-inside">
           <li>
+            <a href="#what-does-this-button-do">What does this button do?</a>
+          </li>
+          <li>
             <a href="#selecting-a-new-pen-hole">Selecting a new pen hole</a>
           </li>
+          <li>
+            <a href="#restoring-purchases">Restoring purchases</a>
+          </li>
         </ul>
+
+        <h2 id="what-does-this-button-do">What does this button do?</h2>
+
+        <p>
+          If you're unsure what a button does, press and hold on the button to
+          reveal a tooltip:
+        </p>
+
+        <img
+          src="help/images/tooltip.gif"
+          alt="A GIF showing how to reveal a tooltip on a button"
+        />
 
         <h2 id="selecting-a-new-pen-hole">Selecting a new pen hole</h2>
 
@@ -27,6 +56,21 @@ export default function Home() {
           src="help/images/select-hole-button.png"
           alt="A screenshot of the 'select hole' button"
         />
+
+        <h2 id="restoring-purchases">Restoring purchases</h2>
+
+        <p>
+          If you purchase premium features and then uninstall/reinstall the app
+          - or if you try to use the premium features on a second device - you
+          will need to click the <b>Restore purchases</b> button in the{' '}
+          <b>Additional options</b> sidebar.
+        </p>
+
+        <p>
+          Please note that purchases cannot be shared across platforms. If you
+          purchase the item on an iOS device, the item will not be available on
+          Android devices, and vice-versa.
+        </p>
       </div>
     </div>
   );
--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -4,11 +4,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily: {
-      display: ['Playfair Display', 'serif'],
-      body: ['Lato', 'ui-sans-serif', 'system-ui', 'sans-serif'],
-    },
     extend: {
+      fontFamily: {
+        display: ['Playfair Display', 'serif'],
+        body: ['Lato', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
       backgroundColor: {
         beige: '#f7efda',
       },
