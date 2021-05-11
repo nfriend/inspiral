73850b2366d5c06d0538766aa9abe1b891bed914
Nathan Friend <nathan@gitlab.com>
2021-05-11 19:28:29 -0400

    docs: add snap points documentation

--- website/pages/help/index.tsx
+++ website/pages/help/index.tsx
@@ -27,11 +27,21 @@ export default function Home() {
             <a href="#selecting-a-new-pen-hole">Selecting a new pen hole</a>
           </li>
           <li>
+            <a href="#using-fixed-gear-snap-points">
+              Using fixed gear "snap points"
+            </a>
+          </li>
+          <li>
             <a href="#restoring-purchases">Restoring purchases</a>
           </li>
           <li>
             <a href="#saving-pictures-to-the-gallery">
               Saving pictures to the gallery
+            </a>
+          </li>
+          <li>
+            <a href="#where-do-pictures-go-when-they-are-saved">
+              Where do pictures go when they are saved?
             </a>
           </li>
         </ul>
@@ -60,6 +70,37 @@ export default function Home() {
         <img
           src="/help/images/select-hole-button.png"
           alt="A screenshot of the 'Select hole' button"
+        />
+
+        <h3 id="using-fixed-gear-snap-points">
+          Using fixed gear "snap points"
+          <div className="version-indicator">Introduced in version 1.1.0</div>
+        </h3>
+
+        <p>
+          When you begin moving the rotating gear, a small blue "snap point" is
+          added to the canvas.
+        </p>
+
+        <img
+          src="/help/images/snap-point.jpg"
+          alt="A gear with a 'snap point' at its center"
+        />
+
+        <p>
+          While moving the fixed gear around the canvas, the fixed gear will
+          "snap" to these points to allow for perfect alignment with previous
+          drawings.
+        </p>
+
+        <p>
+          To turn snap points on or off, press the final button in the "Tools"
+          tab.
+        </p>
+
+        <img
+          src="/help/images/snap-points-button.png"
+          alt="A screenshot of the 'Snap points' toggle button"
         />
 
         <h3 id="restoring-purchases">Restoring purchases</h3>
@@ -102,15 +143,25 @@ export default function Home() {
         </p>
 
         <p>
-          <b>On iOS</b>, press the <b>Done</b> button to save your picture. The
-          picture will be saved to the Photos app.
+          <b>On iOS</b>, press the <b>Done</b> button to save your picture.
         </p>
 
         <p>
-          <b>On Android</b>, press the checkmark icon to save your picture. The
-          picture will be saved to a "Pictures" folder on your device. If you
-          are using Google Photos, this folder can be accessed on the{' '}
-          <b>Photos on device</b> screen.
+          <b>On Android</b>, press the checkmark icon to save your picture.
+        </p>
+
+        <h3 id="where-do-pictures-go-when-they-are-saved">
+          Where do pictures go when they are saved?
+        </h3>
+
+        <p>
+          <b>On iOS</b>, pictures are saved to the Photos app.
+        </p>
+
+        <p>
+          <b>On Android</b>, pictures are saved to a "Pictures" folder on your
+          device. If you are using Google Photos, this folder can be accessed on
+          the <b>Photos on device</b> screen.
         </p>
       </div>
 
